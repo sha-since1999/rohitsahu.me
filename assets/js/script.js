50e648b6e05829e7b9a583a9bc5cde9e154ff283
Rohit Sahu <rohit.sahu@innovaccer.com>
2023-07-08 13:23:12 +0530

    add underconstructionn block| adjust nav

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+var countdownElement = document.getElementById("countdown");
+var countdownDate = new Date("2023-07-31").getTime();
+
+var countdownInterval = setInterval(function() {
+var now = new Date().getTime();
+var distance = countdownDate - now;
+
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+countdownElement.innerText = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+
+if (distance < 0) {
+    clearInterval(countdownInterval);
+    countdownElement.innerText = "Website is now live!";
+    }
+}, 1000);
